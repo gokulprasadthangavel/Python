8c605a7f87afbf0158d28bb9de7cdf8a6a22a676
gokul <gokul@psgsoftwaretechnologies.com>
2019-12-29 12:21:30 +0530

    python code for printing calendar

--- learn-calendar.py
+++ learn-calendar.py
@@ -1,9 +1,13 @@
 import calendar
-# This will print the weekheader eg:- M,T
+# TO print the week header
 print(calendar.weekheader(1))
 print()
-# This will print the first day of the week
-print(calendar.firstweekday())
-print()
 # This will print the calendar
-print(calendar.month(2019, 12))
+print (calendar.calendar(2019))
+# For example if we wanted to print the single month eg:- March
+print (calendar.month(2019, 3))
+# Python's default first day of the calendar is monday, so it starts with monday.
+# If we wanted to start the day with Sunday then, we have to  use setfirstweekday
+calendar.setfirstweekday(calendar.SUNDAY)
+# Now the calendar will be printed with sunday as the first day
+print(calendar.calendar(2019))
