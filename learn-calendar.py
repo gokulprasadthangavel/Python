aab25d56718ccaa4438cab9891bd10152ea25658
gokul <gokul@psgsoftwaretechnologies.com>
2019-12-29 12:03:02 +0530

    print calendar using python

---
+++ learn-calendar.py
@@ -0,0 +1,9 @@
+import calendar
+# This will print the weekheader eg:- M,T
+print(calendar.weekheader(1))
+print()
+# This will print the first day of the week
+print(calendar.firstweekday())
+print()
+# This will print the calendar
+print(calendar.month(2019, 12))
